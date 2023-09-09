cfaf9b6943f8f82879aab64d943421364f2249cb
ackmedtijani <tijanioboiriendev@gmail.com>
2023-09-09 12:23:10 +0100

    Second Commit

--- task1/main.js
+++ task1/main.js
@@ -3,5 +3,15 @@ let current_day = document.querySelector("span[data-testid='currentDayOfTheWeek'
 let utc_time = document.querySelector("span[data-testid='currentUTCTime']");
 const day_of_the_week = ['Sunday' , 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday', 'Saturday'];
 
-utc_time.textContent = dates.toISOString();
 current_day.textContent = day_of_the_week[dates.getDay()];
+utc_time.textContent = dates.getMilliseconds();
+
+
+
+setInterval(function () {
+    dates = new Date();
+    utc_time.textContent = dates.getMilliseconds()
+    
+    ;}, 
+
+1000);
